fd17c100182a531e5bb195b1ea0b535ab7e1da1c
Chris Bahn <csbahn@gmail.com>
2025-03-21 16:59:08 +0100

    Initial Commit

---
+++ Elvis Costello Challenge/Elvis_Costello_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elvis_Costello_ChallengeApp.swift
+//  Elvis Costello Challenge
+//
+//  Created by Chris Bahn on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Elvis_Costello_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
